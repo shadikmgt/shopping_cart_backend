223bbfa4911caa0ae104c5c221d868d464c0c8a5
sadik <shadikmgtbu@gmail.com>
2024-11-13 12:20:07 +0600

    image service implement done.

---
+++ e-ground/src/main/java/com/codinglife/eground/repository/ImageRepository.java
@@ -0,0 +1,8 @@
+package com.codinglife.eground.repository;
+
+import com.codinglife.eground.model.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageRepository extends JpaRepository<Image, Long> {
+
+}
---
+++ e-ground/src/main/java/com/codinglife/eground/service/image/IImageService.java
@@ -0,0 +1,14 @@
+package com.codinglife.eground.service.image;
+
+import com.codinglife.eground.dto.ImageDto;
+import com.codinglife.eground.model.Image;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public interface IImageService {
+    Image getImageById(Long id);
+    void deleteImageBYId(Long id);
+    List<ImageDto> saveImages(List<MultipartFile> files, Long productId);
+    void updateImage(MultipartFile file, Long imageId);
+}
